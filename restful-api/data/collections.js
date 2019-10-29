bce0dcc879698518bc49d494f23b0781defdfe1c
Hitesh Shetty <hitesh.shetty.k90@gmail.com>
2019-10-28 20:40:03 -0400

    Base file commit for data functions.
    Comitting data function files to fetch data from MongoDB.

---
+++ restful-api/data/collections.js
@@ -0,0 +1,25 @@
+const dbConnection = require("./connection");
+
+/* Mongo db connection*/
+const getCollectionFn = collection => {
+    let _col = undefined;
+
+    return async () => {
+        if (!_col) {
+            const db = await dbConnection();
+            _col = await db.collection(collection);
+        }
+
+        return _col;
+    };
+};
+
+/* Listing the collections */
+module.exports = {
+    user: getCollectionFn("user"),
+    booking: getCollectionFn("booking"),
+    hpackages: getCollectionFn("hpackages"),
+	hotels: getCollectionFn("hotels"),
+	locations: getCollectionFn("locations"),
+	itinerary: getCollectionFn("itinerary")
+};
