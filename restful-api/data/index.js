c4b88f7da9e1ae646a242184c455d66c0bc31208
kshvarma333 <57238008+kshvarma333@users.noreply.github.com>
2019-12-07 16:06:46 -0500

    Update index.js

--- restful-api/data/index.js
+++ restful-api/data/index.js
@@ -6,20 +6,19 @@ const connection = require('./connection');
 
 async function create()
 {
-//    const one = await hpackage1.create("kerala", "1500", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
-//    console.log(one);
-//    const two = await hpackage1.create("Himalayas", "2000", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
-//    console.log(two);
-//    const three = await hpackage1.create("TamilNadu", "18000", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
-//    console.log(three);
-//    const four = await hpackage1.create("AndhraPradesh", "1700", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
-//    console.log(four);
-//    const five = await hpackage1.create("Punjab", "1700", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
-//    console.log(five);
-//    const six = await hpackage1.create("WestBengal", "1700", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
-//    console.log(six);
-const seven = await hpackage1.getall();
-console.log(seven);
+   const one = await hpackage1.create("kerala", "1500", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
+   console.log(one);
+   const two = await hpackage1.create("Himalayas", "2000", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
+   console.log(two);
+   const three = await hpackage1.create("TamilNadu", "18000", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
+   console.log(three);
+   const four = await hpackage1.create("AndhraPradesh", "1700", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
+   console.log(four);
+   const five = await hpackage1.create("Punjab", "1700", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
+   console.log(five);
+   const six = await hpackage1.create("WestBengal", "1700", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
+   console.log(six);
+
 
    const db = await connection();
     await db.serverConfig.close();
