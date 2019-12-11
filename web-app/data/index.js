58a433b7fe79a54b43f353c8121ce4164ac30391
namratadg911 <49354732+namratadg911@users.noreply.github.com>
2019-12-11 15:59:47 -0500

    Update index.js

--- web-app/data/index.js
+++ web-app/data/index.js
@@ -7,18 +7,18 @@ const bcrypt = require("bcryptjs");
 
 async function create()
 {
-//    const one = await hpackage1.create("kerala", "1500", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
-//    console.log(one);
-//    const two = await hpackage1.create("Himalayas", "2000", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
-//    console.log(two);
-//    const three = await hpackage1.create("TamilNadu", "18000", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
-//    console.log(three);
-//    const four = await hpackage1.create("AndhraPradesh", "1700", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
-//    console.log(four);
-//    const five = await hpackage1.create("Punjab", "1700", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
-//    console.log(five);
-//    const six = await hpackage1.create("WestBengal", "1700", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
-//    console.log(six);
+   const one = await hpackage1.create("kerala", "1500", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
+   console.log(one);
+   const two = await hpackage1.create("Himalayas", "2000", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
+   console.log(two);
+   const three = await hpackage1.create("TamilNadu", "18000", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
+   console.log(three);
+   const four = await hpackage1.create("AndhraPradesh", "1700", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
+   console.log(four);
+   const five = await hpackage1.create("Punjab", "1700", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
+   console.log(five);
+   const six = await hpackage1.create("WestBengal", "1700", "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201701/kstory_647_010317124538.jpg");
+   console.log(six);
 
    const saltRounds = 5;
 
