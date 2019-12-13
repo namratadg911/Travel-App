7a9ea628a282a2d80e83267a003937af970a06c5
AishwariyaRajendraprasad <49467484+AishwariyaRajendraprasad@users.noreply.github.com>
2019-12-13 14:13:39 -0500

    Added all the itinerary

--- web-app/tasks/itinerary-data.js
+++ web-app/tasks/itinerary-data.js
@@ -9,7 +9,177 @@ const itineraries = [{
         "name": "Day 2",
         "attractions": ["3", "4"],
         "hotel": "2"
+    },
+    {
+        "_id": "3",
+        "name": "Day 3",
+        "attractions": ["5", "6"],
+        "hotel": "3"
+    },
+    {
+        "_id": "4",
+        "name": "Day 4",
+        "attractions": ["7", "8"],
+        "hotel": "4"
+    },
+    {
+        "_id": "5",
+        "name": "Day 5",
+        "attractions": ["9", "10"],
+        "hotel": "5"
+    },
+    {
+        "_id": "6",
+        "name": "Day 1",
+        "attractions": ["11", "12"],
+        "hotel": "6"
+    },
+    {
+        "_id": "7",
+        "name": "Day 2",
+        "attractions": ["13", "14"],
+        "hotel": "7"
+    },
+    {
+        "_id": "8",
+        "name": "Day 3",
+        "attractions": ["15", "16"],
+        "hotel": "8"
+    },
+    {
+        "_id": "9",
+        "name": "Day 4",
+        "attractions": ["17", "18"],
+        "hotel": "9"
+    },
+    {
+        "_id": "10",
+        "name": "Day 5",
+        "attractions": ["19", "20"],
+        "hotel": "10"
+    },
+    {
+        "_id": "11",
+        "name": "Day 1",
+        "attractions": ["21", "22"],
+        "hotel": "11"
+    },
+    {
+        "_id": "12",
+        "name": "Day 2",
+        "attractions": ["23", "24"],
+        "hotel": "12"
+    },
+    {
+        "_id": "13",
+        "name": "Day 3",
+        "attractions": ["25", "26"],
+        "hotel": "13"
+    },
+    {
+        "_id": "14",
+        "name": "Day 4",
+        "attractions": ["27", "28"],
+        "hotel": "14"
+    },
+    {
+        "_id": "15",
+        "name": "Day 5",
+        "attractions": ["29", "30"],
+        "hotel": "15"
+    },
+    {
+        "_id": "16",
+        "name": "Day 1",
+        "attractions": ["31", "32"],
+        "hotel": "16"
+    },
+    {
+        "_id": "17",
+        "name": "Day 2",
+        "attractions": ["33", "34"],
+        "hotel": "17"
+    },
+    {
+        "_id": "18",
+        "name": "Day 3",
+        "attractions": ["35", "36"],
+        "hotel": "18"
+    },
+    {
+        "_id": "19",
+        "name": "Day 4",
+        "attractions": ["37", "38"],
+        "hotel": "19"
+    },
+    {
+        "_id": "20",
+        "name": "Day 5",
+        "attractions": ["39", "40"],
+        "hotel": "20"
+    },
+    {
+        "_id": "21",
+        "name": "Day 1",
+        "attractions": ["41", "42"],
+        "hotel": "21"
+    },
+    {
+        "_id": "22",
+        "name": "Day 2",
+        "attractions": ["43", "44"],
+        "hotel": "22"
+    },
+    {
+        "_id": "23",
+        "name": "Day 3",
+        "attractions": ["45"],
+        "hotel": "23"
+    },
+    {
+        "_id": "24",
+        "name": "Day 4",
+        "attractions": ["46"],
+        "hotel": "24"
+    },
+    {
+        "_id": "25",
+        "name": "Day 5",
+        "attractions": ["47"],
+        "hotel": "25"
+    },
+    {
+        "_id": "26",
+        "name": "Day 1",
+        "attractions": ["48", "49"],
+        "hotel": "26"
+    },
+    {
+        "_id": "27",
+        "name": "Day 2",
+        "attractions": ["50", "51"],
+        "hotel": "27"
+    },
+    {
+        "_id": "28",
+        "name": "Day 3",
+        "attractions": ["52","53"],
+        "hotel": "28"
+    },
+    {
+        "_id": "29",
+        "name": "Day 4",
+        "attractions": ["54","55"],
+        "hotel": "29"
+    },
+    {
+        "_id": "30",
+        "name": "Day 5",
+        "attractions": ["56","57"],
+        "hotel": "30"
     }
+
+
 ];
 
 function getItineraries() {
